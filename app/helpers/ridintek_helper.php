4e66d84c064350890bff7d2a6989e4d44ddc519f
ridintek <rw056h@gmail.com>
2022-12-14 13:11:37 +0700

    Optimize created_by algorithm.

--- app/helpers/ridintek_helper.php
+++ app/helpers/ridintek_helper.php
@@ -2674,19 +2674,9 @@ function sendWA($phone, $text, $opt = [])
 function setCreatedBy($data)
 {
   $data['created_at'] = ($data['date'] ?? $data['created_at'] ?? date('Y-m-d H:i:s'));
+  $system = User::getRow(['username' => 'system']);
 
-  if (!empty($data['created_by'])) {
-    if ($creator = User::getRow(['id' => $data['created_by']])) {
-      $data['created_by'] = $creator->id;
-    }
-  } else {
-    if ($creatorId = XSession::get('user_id')) {
-      $data['created_by'] = $creatorId;
-    } else {
-      $user = User::getRow(['username' => 'system']);
-      $data['created_by'] = $user->id;
-    }
-  }
+  $data['created_by'] = ($data['created_by'] ?? XSession::get('user_id') ?? $system->id);
 
   return $data;
 }
