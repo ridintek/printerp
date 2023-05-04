1d6eaa2fbc4ed5ac6202df1d372cc6121f91c6f2
ridintek <rw056h@gmail.com>
2023-05-04 09:20:18 +0700

    Fix undefined saleitem variable.

--- app/controllers/admin/Sales.php
+++ app/controllers/admin/Sales.php
@@ -1635,8 +1635,8 @@ class Sales extends MY_Controller
             $excel->setCellValue('D' . $b, ($saleItemJS->due_date ?? ''));
             $excel->setCellValue('E' . $b, ($saleItemJS->completed_at ?? ''));
             $excel->setCellValue('F' . $b, $duration);
-            $excel->setCellValue('G' . $b, ($saleItem->status != 'need_payment' && $saleItem->status != 'draft' ? $timeleft : ''));
-            $excel->setCellValue('H' . $b, ($saleItem->status != 'need_payment' && $saleItem->status != 'draft' ? $overdue : ''));
+            $excel->setCellValue('G' . $b, ($item->status != 'need_payment' && $item->status != 'draft' ? $timeleft : ''));
+            $excel->setCellValue('H' . $b, ($item->status != 'need_payment' && $item->status != 'draft' ? $overdue : ''));
             $excel->setCellValue('I' . $b, strtoupper($op_username));
             $excel->setCellValue('J' . $b, $op_name);
             $excel->setCellValue('K' . $b, ($isOperator ? 'OP' : 'NON OP'));
@@ -1647,7 +1647,7 @@ class Sales extends MY_Controller
             $excel->setCellValue('P' . $b, $item->product_name);
             $excel->setCellValue('Q' . $b, $item->quantity);
             $excel->setCellValue('R' . $b, filterDecimal($item->subtotal));
-            $excel->setCellValue('S' . $b, lang($saleItem->status));
+            $excel->setCellValue('S' . $b, lang($item->status));
 
             $b++;
           }
